caefacd08e4017fc08a770d48fc4ed98fee40b38
beesham <sarendranauth.beesham@gmail.com>
2019-01-17 15:04:49 -0500

    Initial Commit

---
+++ src/PersistentNumbers.c
@@ -0,0 +1,20 @@
+/*
+* Author: Beesham Sarendranauth | 104854956
+* Date: 2019/01/24
+* Class: Operating Systems Fundamentals (COMP3300)
+* Description: This program takes a file with a list of numbers
+* that. The persistency of each number will be calculated and the max and min persitent will 
+* be printed. The program will also calulate the execution time 
+*/
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+
+
+int main(void) {
+
+
+}
