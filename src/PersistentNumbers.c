0f7f53b53ffa1340ec6f27ce993b8be986eb6eb9
beesham <sarendranauth.beesham@gmail.com>
2019-01-17 20:46:15 -0500

    Feat: Read file into array

--- src/PersistentNumbers.c
+++ src/PersistentNumbers.c
@@ -6,6 +6,7 @@
 * Description: This program takes a file with a list of numbers
 * that. The persistency of each number will be calculated and the max and min persitent will 
 * be printed. The program will also calulate the execution time 
+* Assumptions: a number is 9 digits long and is line separated
 */
 
 
@@ -14,13 +15,37 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <stdbool.h>
+#include <fcntl.h>
+
+#define SIZE_OF_ARRAY 10
 
 bool argumentCheck(); //check if any args were provided
+bool readFile(); //reads the file for list of numbers
+void sort(); //sorts the input 
+void calculatePersitent(); //calculates the persistency of the number
 
-int main(int argc, char *argv[]) {
+
+int main(int argc, char *argv[]) {  
+    int numbers[SIZE_OF_ARRAY];   //array to hold the list of numbers
     
-    if (!argumentCheck(argc)) exit(0);
+    if (!argumentCheck(argc) || !readFile(numbers, argv)) exit(0);
     
+}
+
+bool readFile(int *array, char *file[]) {
+    FILE *fp;
+    
+    //Open file
+    if((fp = fopen(file[1], "r")) == NULL) {
+        printf("Error opening file\n");
+        return false;
+    }
+    
+    //Reads the numbers into the array
+    int n=0;
+    while(fscanf(fp, "%d", &array[n]) != EOF) n++;
+        
+    return true;
 }
 
 bool argumentCheck(int argc) {
